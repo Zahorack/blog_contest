7d72c3f0f329bae316397065c9a9a6488ae53978
Oliver Holly <oliverholly11@gmail.com>
2020-06-11 21:12:01 +0200

    Implemented version 1

--- string/src/contest_string.cpp
+++ string/src/contest_string.cpp
@@ -1,41 +1,115 @@
 #include <contest_string.h>
+#include <iostream>
 
 namespace contest
 {
 
 	int memcmp(const void *left, const void *right, size_t byteLength)
 	{
+	    const unsigned char *p_src = (const unsigned char *)left;
+        const unsigned char *p_dest = (const unsigned char *)right;
+
+        if(left == nullptr || right == nullptr)
+            return -2;
+
+	    for(; byteLength--; p_src++, p_dest++) {
+	        if(*p_src < *p_dest)
+	            return -1;
+            else if(*p_src > *p_dest)
+                return 1;
+
+	    }
 		return 0;
 	}
 
 	void *memcpy(void *_Rstr dest, const void *_Rstr src, size_t byteLength)
 	{
-		return nullptr;
+        if(dest == nullptr || src == nullptr || byteLength == 0)
+            return  dest;
+
+        const unsigned char *p_src = (const unsigned char *)src;
+        unsigned char *p_dest = (unsigned char *)dest;
+
+        for(int i = 0; i < byteLength; i++) {
+            *(p_dest + i) = *(p_src + i);
+        }
+
+        return dest;
 	}
 
 	void *memmove(void *dest, const void *src, size_t byteLength)
 	{
-		return nullptr;
+        const unsigned char *p_src = (const unsigned char *)src;
+        unsigned char *p_dest = (unsigned char *)dest;
+
+        if (p_src > p_dest)
+            memcpy(dest, src, byteLength);
+        else
+            for (p_dest += byteLength, p_src += byteLength; byteLength--;)
+                *--p_dest = *--p_src;
+
+        return dest;
 	}
 
 	void *memset(void *dest, int value, size_t byteLength)
 	{
-		return nullptr;
+
+        if(dest == nullptr ||  byteLength == 0)
+            return dest;
+
+        unsigned char *p_dest = (unsigned char *)dest;
+
+        for(int i = 0; i < byteLength; i++) {
+            *(p_dest + i) = value;
+        }
+
+        return dest;
 	}
 
 	char *strcpy(char *_Rstr dest, const char *_Rstr src)
 	{
-		return nullptr;
+        if(dest == nullptr || src == nullptr)
+            return  dest;
+
+        size_t length = strlen(src);
+
+        const char *p_src = (const char *)src;
+        char *p_dest = (char *)dest;
+
+        for(int i = 0; i < length; i++) {
+            *(p_dest + i) = *(p_src + i);
+        }
+        *(p_dest + length) = '\0';
+
+        return dest;
 	}
 
 	char *strncpy(char *_Rstr dest, const char *_Rstr src, size_t length)
 	{
-		return nullptr;
+        if(dest == nullptr || src == nullptr)
+            return  dest;
+
+        const char *p_src = (const char *)src;
+        char *p_dest = (char *)dest;
+
+        for(int i = 0; i < length; i++) {
+            *(p_dest + i) = *(p_src + i);
+        }
+        *(p_dest + length) = '\0';
+
+        return dest;
 	}
 
 	size_t strlen(const char *str)
 	{
-		return 0;
+	    if(str == nullptr)
+	        return 0;
+
+        const char *p_begin = str;
+        while(*str != '\0') {
+            str++;
+        }
+        return size_t(str - p_begin);
 	}
 
 }
--- string_test.cpp
+++ string_test.cpp
@@ -3,28 +3,28 @@
 
 int main()
 {
-	std::cout
-			<< "This is a fork of https://www.github.com/xgallom/blog_contest\n"
-			<< "\n"
-			<< "Goal of the contest is to implement functions as declared in string/include/contest_string.h\n"
-			<< "Implementation skeleton is in string/src/contest_string.c\n"
-			<< "\n"
-			<< "This project produces a static library string and an executable string_test that you can change\n"
-			<< "in /string_test.cpp. Feel free to test your library there. I will be checking it by myself for\n"
-			<< "mistakes and will not look into you string_test.cpp file.\n"
-			<< "\n"
-			<< "The implementation should not depend on any standard library functions, and should work in\n"
-			<< "freestanding environments. If they don\'t due to some weird niche thing and they work in hosted,\n"
-			<< "it\'s still a valid submission, so you do not have to test it outside this project.\n"
-			<< "\n"
-			<< "Consult https://en.cppreference.com/w/cpp/header/cstring for how the functions should behave.\n"
-			<< "Only functions required to participate in the contest are the ones declared in contest_string.h.\n"
-			<< "\n"
-			<< "DO NOT CHEAT. SERIOUSLY I KNOW HOW TO FIND OUT. ASSHOLE.\n"
-			<< "\n"
-			<< "And good luck, with lots of love <3\n"
-			<< "Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>\n";
-	
+//	std::cout
+//			<< "This is a fork of https://www.github.com/xgallom/blog_contest\n"
+//			<< "\n"
+//			<< "Goal of the contest is to implement functions as declared in string/include/contest_string.h\n"
+//			<< "Implementation skeleton is in string/src/contest_string.c\n"
+//			<< "\n"
+//			<< "This project produces a static library string and an executable string_test that you can change\n"
+//			<< "in /string_test.cpp. Feel free to test your library there. I will be checking it by myself for\n"
+//			<< "mistakes and will not look into you string_test.cpp file.\n"
+//			<< "\n"
+//			<< "The implementation should not depend on any standard library functions, and should work in\n"
+//			<< "freestanding environments. If they don\'t due to some weird niche thing and they work in hosted,\n"
+//			<< "it\'s still a valid submission, so you do not have to test it outside this project.\n"
+//			<< "\n"
+//			<< "Consult https://en.cppreference.com/w/cpp/header/cstring for how the functions should behave.\n"
+//			<< "Only functions required to participate in the contest are the ones declared in contest_string.h.\n"
+//			<< "\n"
+//			<< "DO NOT CHEAT. SERIOUSLY I KNOW HOW TO FIND OUT. ASSHOLE.\n"
+//			<< "\n"
+//			<< "And good luck, with lots of love <3\n"
+//			<< "Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>\n";
+
 
 	contest::memcmp(nullptr, nullptr, 0);
 	contest::memcpy(nullptr, nullptr, 0);
@@ -33,6 +33,44 @@ int main()
 	contest::strcpy(nullptr, nullptr);
 	contest::strncpy(nullptr, nullptr, 0);
 	contest::strlen(nullptr);
+
+
+
+    const char s1[] = "bbcdefg";
+    const char *s2 = "abcdefghdss";
+
+    std::cout<<s1<<"\n";
+    std::cout<<s2<<"\n";
+    std::cout<< contest::strlen(s1)<<"\n";
+    std::cout<< contest::strlen(s2)<<"\n";
+
+    std::cout<<contest::memcmp(s1, s2, contest::strlen(s1))<<"\n";
+
+    char s3[20] = {};
+
+    contest::memcpy(s3, s2, contest::strlen(s2));
+    std::cout<<s3<<"\n";
+
+    contest::memmove(s3, s1, contest::strlen(s1));
+    std::cout<<s3<<"\n";
+
+    contest::memset(s3, 97, contest::strlen(s3)/2);
+    std::cout<<s3<<"\n";
+
+
+    char s4[6] = {'i', 'j', 'k', '\0', 'l', 'm'};
+
+    std::cout<<"s4 len: "<<contest::strlen(s4)<<"\n";
+
+    contest::strcpy(s3, s4);
+    std::cout<<"strcpy: "<<s3<<"\n";
+
+    contest::strncpy(s3, s4, 2);
+    std::cout<<"strncpy: "<<s3<<"\n";
+
+    contest::memcpy(s3, s4, 6);
+    std::cout<<"memcpy: "<<s3<<"\n";
+
 
 	return 0;
 }
