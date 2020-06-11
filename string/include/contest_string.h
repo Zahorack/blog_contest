7a4df9256c70ba7fc4d5b97a0a37c14e7d64b519
Oliver Holly <oliverholly11@gmail.com>
2020-06-11 21:31:35 +0200

    Fix whitespaces

--- string/include/contest_string.h
+++ string/include/contest_string.h
@@ -17,7 +17,7 @@ namespace contest
 	// Consult https://en.cppreference.com/w/cpp/header/cstring for how the functions should behave
 	// Only functions required to participate in the contest are the ones here:
 
-	int memcmp(const void *left, const void *right, size_t byteLength);
+        int memcmp(const void *left, const void *right, size_t byteLength);
 	void *memcpy(void *_Rstr dest, const void *_Rstr src, size_t byteLength);
 	void *memmove(void *dest, const void *src, size_t byteLength);
 	void *memset(void *dest, int value, size_t byteLength);
--- string/src/contest_string.cpp
+++ string/src/contest_string.cpp
@@ -4,112 +4,111 @@
 namespace contest
 {
 
-	int memcmp(const void *left, const void *right, size_t byteLength)
-	{
-	    const unsigned char *p_src = (const unsigned char *)left;
-        const unsigned char *p_dest = (const unsigned char *)right;
+        int memcmp(const void *left, const void *right, size_t byteLength)
+        {
+                const unsigned char *p_src = static_cast<const unsigned char *>(left);
+                const unsigned char *p_dest = static_cast<const unsigned char *>(right);
 
-        if(left == nullptr || right == nullptr)
-            return -2;
+                if(left == nullptr || right == nullptr)
+                        return -2;
 
-	    for(; byteLength--; p_src++, p_dest++) {
-	        if(*p_src < *p_dest)
-	            return -1;
-            else if(*p_src > *p_dest)
-                return 1;
-
-	    }
-		return 0;
+                for(; byteLength--; p_src++, p_dest++) {
+                        if(*p_src < *p_dest)
+                                return -1;
+                        else if(*p_src > *p_dest)
+                                return 1;
+                }
+                return 0;
 	}
 
-	void *memcpy(void *_Rstr dest, const void *_Rstr src, size_t byteLength)
-	{
-        if(dest == nullptr || src == nullptr || byteLength == 0)
-            return  dest;
+        void *memcpy(void *_Rstr dest, const void *_Rstr src, size_t byteLength)
+        {
+                if(dest == nullptr || src == nullptr || byteLength == 0)
+                        return  dest;
 
-        const unsigned char *p_src = (const unsigned char *)src;
-        unsigned char *p_dest = (unsigned char *)dest;
+                const unsigned char *p_src = (const unsigned char *)src;
+                unsigned char *p_dest = (unsigned char *)dest;
 
-        for(int i = 0; i < byteLength; i++) {
-            *(p_dest + i) = *(p_src + i);
+                for(int i = 0; i < byteLength; i++) {
+                        *(p_dest + i) = *(p_src + i);
+                }
+
+                return dest;
         }
 
-        return dest;
-	}
+        void *memmove(void *dest, const void *src, size_t byteLength)
+        {
+                const unsigned char *p_src = (const unsigned char *)src;
+                unsigned char *p_dest = (unsigned char *)dest;
 
-	void *memmove(void *dest, const void *src, size_t byteLength)
-	{
-        const unsigned char *p_src = (const unsigned char *)src;
-        unsigned char *p_dest = (unsigned char *)dest;
+                if (p_src > p_dest)
+                        memcpy(dest, src, byteLength);
+                else
+                        for (p_dest += byteLength, p_src += byteLength; byteLength--;)
+                                *--p_dest = *--p_src;
 
-        if (p_src > p_dest)
-            memcpy(dest, src, byteLength);
-        else
-            for (p_dest += byteLength, p_src += byteLength; byteLength--;)
-                *--p_dest = *--p_src;
-
-        return dest;
-	}
-
-	void *memset(void *dest, int value, size_t byteLength)
-	{
-
-        if(dest == nullptr ||  byteLength == 0)
-            return dest;
-
-        unsigned char *p_dest = (unsigned char *)dest;
-
-        for(int i = 0; i < byteLength; i++) {
-            *(p_dest + i) = value;
+                return dest;
         }
 
-        return dest;
-	}
+        void *memset(void *dest, int value, size_t byteLength)
+        {
+                if(dest == nullptr ||  byteLength == 0)
+                        return dest;
 
-	char *strcpy(char *_Rstr dest, const char *_Rstr src)
-	{
-        if(dest == nullptr || src == nullptr)
-            return  dest;
+                unsigned char *p_dest = (unsigned char *)dest;
 
-        size_t length = strlen(src);
+                for(int i = 0; i < byteLength; i++) {
+                        *(p_dest + i) = value;
+                }
 
-        const char *p_src = (const char *)src;
-        char *p_dest = (char *)dest;
-
-        for(int i = 0; i < length; i++) {
-            *(p_dest + i) = *(p_src + i);
+                return dest;
         }
-        *(p_dest + length) = '\0';
 
-        return dest;
-	}
+        char *strcpy(char *_Rstr dest, const char *_Rstr src)
+        {
+                if(dest == nullptr || src == nullptr)
+                return  dest;
 
-	char *strncpy(char *_Rstr dest, const char *_Rstr src, size_t length)
-	{
-        if(dest == nullptr || src == nullptr)
-            return  dest;
+                size_t length = strlen(src);
 
-        const char *p_src = (const char *)src;
-        char *p_dest = (char *)dest;
+                const char *p_src = (const char *)src;
+                char *p_dest = (char *)dest;
 
-        for(int i = 0; i < length; i++) {
-            *(p_dest + i) = *(p_src + i);
+                for(int i = 0; i < length; i++) {
+                        *(p_dest + i) = *(p_src + i);
+                }
+                *(p_dest + length) = '\0';
+
+                return dest;
         }
-        *(p_dest + length) = '\0';
 
-        return dest;
-	}
+        char *strncpy(char *_Rstr dest, const char *_Rstr src, size_t length)
+        {
+                if(dest == nullptr || src == nullptr)
+                        return  dest;
 
-	size_t strlen(const char *str)
-	{
-	    if(str == nullptr)
-	        return 0;
+                const char *p_src = (const char *)src;
+                char *p_dest = (char *)dest;
 
-        const char *p_begin = str;
-        while(*str != '\0') {
-            str++;
+                for(int i = 0; i < length; i++) {
+                        *(p_dest + i) = *(p_src + i);
+                }
+                *(p_dest + length) = '\0';
+
+                return dest;
         }
-        return size_t(str - p_begin);
-	}
+
+        size_t strlen(const char *str)
+        {
+                if(str == nullptr)
+                        return 0;
+
+                const char *p_begin = str;
+                while(*str != '\0') {
+                        str++;
+                }
+                
+                return size_t(str - p_begin);
+        }
 
 }
